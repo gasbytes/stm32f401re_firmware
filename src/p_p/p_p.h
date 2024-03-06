829372716a781091aca9ea1159bbf5c0b1491d1c
gasbytes <reda.chouk.01@gmail.com>
2024-03-06 23:43:41 +0100

    arch.png for the design and initial sketching

---
+++ src/p_p/p_p.h
@@ -0,0 +1,14 @@
+#ifndef P_P_H
+#define P_P_H
+
+/*
+ * Struct that holds the
+ * fields for the packets of the 
+ * p_p packet protocol.
+ * For details, you can look at the 
+ * arch.png image.
+ * */
+typedef struct packet_t {
+}packet_t;
+
+#endif // !P_P_H
